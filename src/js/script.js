89f3cee87eb56920e98596951eed71009a144580
Serovikov17 <serovikov_kirill@mail.ru>
2021-04-23 17:38:44 +0500

    catalogStart

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
 
+    const items = document.querySelector('.categories__items'),
+          passenger = document.querySelector('#passenger'),
+          choice = document.querySelector('.categories__items-choice'),
+          divider = document.querySelectorAll('.divider');
+     
+    passenger.addEventListener('click', () => {
+        items.classList.remove('active');
+        choice.classList.add('active');
+        divider[0].classList.add('divider_choice');
+        divider[1].classList.add('divider_choice');
+    });
+
+        
+
+    
 });
